7482225fbacf4788fdd04603a4baace8913b9d17
Jack Wu <jack.wuweimin@gmail.com>
2013-10-20 00:44:54 -0700

    prime1

--- src/onlinejudgeproblems/sphere/classical/problemCodePRIME1.java
+++ src/onlinejudgeproblems/sphere/classical/problemCodePRIME1.java
@@ -20,14 +20,40 @@ class problemCodePRIME1 {
 
 		int data_size = Integer.parseInt(in.nextLine());
 
-		while (in.hasNext()) {
+		for(int i = 0; i < data_size; i++){
 			String next = in.nextLine();
 			String[] next_items = next.split(" ");
 
 			int x = Integer.parseInt(next_items[0]);
 			int y = Integer.parseInt(next_items[1]);
+			sieveOfEratoshenes(x, y);
 
 		}
+	}
+	
+	public static void sieveOfEratoshenes(int begin,int end){
+		
+		boolean[] booleanArray = new boolean[end+1];
+		
+		for(int i = 0; i<=end; i++){
+			booleanArray[i] = true;
+		}
+		
+		for(int i= 2; i*i <=end; i++){
+			if(booleanArray[i]){
+				for(int j = i; i*j<=end; j++)
+					booleanArray[i*j] = false;
+			}
+		}
+		for(int i = begin; i < booleanArray.length;i++){
+			if(booleanArray[i])
+				System.out.print(i + " ");
+			else
+				System.out.print("-1" + " ");
+		}
+		
+		System.out.println();
+		
 	}
 
 }
