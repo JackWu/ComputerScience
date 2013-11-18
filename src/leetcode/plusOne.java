f2c39ad124d4b1c51a446889783297d518ddb971
JackWu <jack.wuweimin@gmail.com>
2013-11-18 09:17:25 -0800

    plusOnw

--- src/leetcode/plusOne.java
+++ src/leetcode/plusOne.java
@@ -1,9 +1,18 @@
 package leetcode;
 
+import java.util.ArrayList;
+
 public class plusOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int[] x = {9,9,9,9};
+		int[] ret = plusOne(x);
+		
+		for(int y : ret){
+			System.out.print(y + " ");
+		}
 
 	}
 	
@@ -15,13 +24,37 @@ public class plusOne {
         for(int i = digits.length-1; i >= 0; i--){
         
         	
-    		if(checkHasCarry(digits[i])){
-    			digits[i] = 0;
-    			hasCarry = true;
-    		}else{
-    			digits[i] = digits[i]+1;
-    			hasCarry = false;
-    		}
+        	if(i== digits.length-1){
+        		if(checkHasCarry(digits[i])){
+        			digits[i] = 0;
+        			hasCarry = true;
+        			continue;
+        		}else{
+        			digits[i] = digits[i]+1;
+        			hasCarry = false;
+        		}
+        	}
+        	if(hasCarry){
+        		if(checkHasCarry(digits[i])){
+        			digits[i] = 0;
+        			hasCarry = true;
+        		}else{
+        			digits[i] = digits[i]+1;
+        			hasCarry = false;
+        		}
+        		
+        	}
+        }
+        
+        if(hasCarry){
+        	int[] newDigits = new int[digits.length+1];
+        	newDigits[0]= 1;
+        	int newDigitsIndex = 1;
+        	for(int tempInt: digits){
+        		newDigits[newDigitsIndex] = tempInt;
+        		newDigitsIndex++;
+        	}
+	        digits = newDigits;
         }
         
         return digits;
